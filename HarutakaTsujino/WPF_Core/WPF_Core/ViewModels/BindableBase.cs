9f469057b57a7d0a9c78988f1b40d302c4e6a157
Harutaka-Tsujino <glaca986@outlook.jp>
2019-12-29 19:00:58 +0900

    BindableBaseの実装

---
+++ HarutakaTsujino/WPF_Core/WPF_Core/ViewModels/BindableBase.cs
@@ -0,0 +1,25 @@
+﻿using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace WPF_Core.ViewModels
+{
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected bool SetValue<T>(ref T variable, T value, [CallerMemberName] string? propertyName = null)
+        {
+            if (Equals(variable, value)) return false;
+
+            variable = value;
+            RaisePropertyChanged(propertyName);
+
+            return true;
+        }
+
+        protected void RaisePropertyChanged([CallerMemberName] string? propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
