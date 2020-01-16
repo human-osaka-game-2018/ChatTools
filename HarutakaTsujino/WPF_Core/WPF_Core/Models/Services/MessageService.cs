a06a1efae6b1f9188477c2931d36c6e78dde1a43
Harutaka-Tsujino <glaca986@outlook.jp>
2020-01-16 14:11:55 +0900

    メッセージの裏読み

--- HarutakaTsujino/WPF_Core/WPF_Core/Models/Services/MessageService.cs
+++ HarutakaTsujino/WPF_Core/WPF_Core/Models/Services/MessageService.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 using System.Reactive;
+using System.Reactive.Linq;
 using System.Reactive.Subjects;
+using System.Threading.Tasks;
 using WPF_Core.Infrastructure.Database;
 using WPF_Core.Models.DomainObjects;
 using WPF_Core.Models.Services.Factories;
@@ -13,6 +15,19 @@ namespace WPF_Core.Models.Services
     static class MessageService
     {
         public static IObservable<Unit> OnMessagePostedAsObservable => onMessagePostedAsSubject;
+
+        static MessageService()
+        {
+            ChannelService.OnSelectingChangedAsObservable
+                .Subscribe((channel) =>
+                {
+                    if (channel is null) return;
+
+                    onMessagesFetchedAsSubject.OnNext(Get(channel));
+                });
+
+            _ = FetchMessagesContinuousAsync();
+        }
 
         public static IEnumerable<Message?> Get(Channel channel)
         {
@@ -30,6 +45,29 @@ namespace WPF_Core.Models.Services
 
             onMessagePostedAsSubject.OnNext(Unit.Default);
         }
+
+        private static async Task FetchMessagesContinuousAsync()
+        {
+            while (true)
+            {
+                // UIスレッドで扱う必要のあるメソッドがあるためConfigureAwaitは使わない
+                await Task.Run(() => System.Threading.Thread.Sleep((int)(MESSAGE_UPDATE_TIME_SPAN_S * 1000)));
+
+                var messagesTask = await Task.Run(() =>
+                {
+                    var currentChannel = ChannelService.SelectingChannel;
+
+                    return currentChannel is null ? new List<Message?>() : Get(currentChannel);
+                });
+
+                onMessagesFetchedAsSubject.OnNext(messagesTask);
+            }
+        }
+
+        private const double MESSAGE_UPDATE_TIME_SPAN_S = 1.0;
+
+        private static readonly BehaviorSubject<IEnumerable<Message?>> onMessagesFetchedAsSubject =
+            new BehaviorSubject<IEnumerable<Message?>>(new List<Message?>());
 
         private static readonly Subject<Unit> onMessagePostedAsSubject = new Subject<Unit>();
     }
