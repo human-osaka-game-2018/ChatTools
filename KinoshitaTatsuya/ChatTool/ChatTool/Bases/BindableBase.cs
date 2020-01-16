bf1cc717052f656c1092063c500e0173c9571a62
Kinoshita-Tatsuya <t.kinoshita@gmail.com>
2020-01-16 11:40:32 +0900

    INotifyPropertyChangedを実装した基底クラスを実装

---
+++ KinoshitaTatsuya/ChatTool/ChatTool/Bases/BindableBase.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Text;
+
+namespace ChatTool.Bases
+{
+    public abstract class BindableBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void OnPropertyChanged([CallerMemberName]string? propertyName = null)
+        {                        
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        protected void SetProperty<T>(ref T strage,T value,[CallerMemberName]string? propertyName = null)
+        {
+            if (object.Equals(strage, value)) return;
+
+            strage = value;
+            OnPropertyChanged(propertyName);
+        }
+    }
+}
